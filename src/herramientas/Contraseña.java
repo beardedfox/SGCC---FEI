2d4f70cb4fbc1273d9e97c9e1b1a97e3c438178b
Unknown <albertoj1606@gmail.com>
2018-12-10 08:57:03 -0600

    Herramientas
    Clases de utilidad

---
+++ src/herramientas/Contraseña.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herramientas;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ *
+ * @author Alberto Sánchez
+ */
+public class Contraseña {
+
+  private static final int COMPLEJIDAD = 12;
+
+  public String devuelveContraseñaHasheada(String contraseña) {
+    String contraseñaHasheada = BCrypt.hashpw(contraseña, BCrypt.gensalt(COMPLEJIDAD));
+    return contraseñaHasheada;
+  }
+  
+  public boolean evaluaSiExiste(String contraseñaCandidata, String contraseñaHasheada){
+    if (BCrypt.checkpw(contraseñaCandidata, contraseñaHasheada) == true) {
+      return true;
+    }
+    return false;
+  }
+}
